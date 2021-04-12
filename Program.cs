d7dd8ca39702f8b8109b1b7fb307bc06621ff64d
samhowes <sam@samhowes.com>
2021-04-12 19:30:47 -0400

    dotnet new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace overrides_repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
